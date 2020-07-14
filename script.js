12a3934a2e4a622955d78746ea3ac8154c90e4f2
olakv <kveola13@student.westerdals.no>
2020-07-14 08:35:47 +0200

    Challenges

---
+++ script.js
@@ -0,0 +1,22 @@
+function calcAge(year){
+	console.log(2020 - year)
+}
+
+calcAge(1992)
+
+let retirement = function (year) {
+	console.log(67 - (2020-year))
+}
+
+retirement(1992)
+
+console.log(age)
+var age = 28
+
+
+function foo(){
+	var age = 67
+	console.log(age)
+}
+foo()
+console.log(age)
